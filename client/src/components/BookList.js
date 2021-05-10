d9015a280f25d1c9b69da1ef5b7441987f81f93f
Thanawat J <thanawat.j@gli.co.th>
2021-05-10 11:17:32 +0700

    add Book comp

---
+++ client/src/components/BookList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+const BookList = ({ data }) => {
+  const displayBooks = () => {
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map((book) => <li key={book.id}>{book.name}</li>);
+    }
+  };
+
+  return (
+    <div>
+      <ul id="book-list">{displayBooks()}</ul>
+    </div>
+  );
+};
+
+export default graphql(getBooksQuery)(BookList);
